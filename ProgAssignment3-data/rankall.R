acb4c3fd2d0985b593c51eadb8c9653db342245d
thatyang <yangsuiny@gmail.com>
2013-10-14 18:24:08 -0400

    Part of rankall.R

---
+++ ProgAssignment3-data/rankall.R
@@ -0,0 +1,34 @@
+rankall<-function(outcome,num='best'){
+    hpfile<-read.csv('outcome-of-care-measures.csv',colClasses='character')
+    listofstate<-unique(hpfile[,7])
+    resultset<-data.frame(matrix(NA, ncol = 2))
+    if (outcome %in% c('heart attack','heart failure','pneumonia')){
+        for (state in listofstate){
+            stateset<-subset(hpfile,State==state)
+            if (outcome=='heart attack' && !stateset[,11] %in% 'Not Available'){
+               # print(state)
+                statesetfull<-subset(stateset,!stateset[,11] %in% 'Not Available')
+                haset<-as.numeric(statesetfull[,11])
+                set_rank<-cbind(factor(haset),statesetfull[,2])
+                set_rank_sorted<-set_rank[order(as.numeric(set_rank[,1]),set_rank[,2]),]
+                #print(paste(state,' ',nrow(set_rank_sorted),sep=''))
+                if (!is.null(nrow(set_rank_sorted)) && length(set_rank_sorted)>0) {
+#                   print(nrow(set_rank_sorted))
+#                   print(length(set_rank_sorted))
+                    if (nrow(set_rank_sorted)>=num){
+                         set_rank_sorted_last<-cbind(1:nrow(set_rank_sorted),set_rank_sorted,state)
+                         colnames(set_rank_sorted_last)<-c('rank','temprank','hospital','state')
+                         result<-set_rank_sorted_last[set_rank_sorted_last[,'rank']==num,3:4]
+                         resultset<-rbind(resultset,result)
+                     }
+                     else {
+                        result<-c('NA',state)
+                        resultset<-rbind(resultset,result)
+                     }
+                }
+            }
+        }
+    resultsetnew<-subset(resultset,complete.cases(resultset))
+    return(resultsetnew)
+    }
+}

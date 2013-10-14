c525d4a8d6908642193735383fdb49f9a7f27a80
thatyang <yangsuiny@gmail.com>
2013-10-14 12:08:45 -0400

    Modified best.R and rankhospital.R

--- ProgAssignment3-data/best.R
+++ ProgAssignment3-data/best.R
@@ -1,28 +1,28 @@
 best<-function(state,outcome){
-    hpfile<-read.csv('outcome-of-care-measures.csv',colClasses='character')
+    hpfile<-read.csv('outcome-of-care-measures.csv',colclasses='character')
     listofstate<-unique(hpfile[,7])
 #   print(listofstate)
 #   print(state)
     if (state %in% listofstate && outcome %in% c('heart attack','heart failure','pneumonia')
  ){
-    stateset<-subset(hpfile,State==state)
+    stateset<-subset(hpfile,state==state)
 #   print('valid state and outcome')
        if (outcome=='heart attack'){
             outcomeha<-stateset[,11]
-            outcomehaclean<-subset(outcomeha,!outcomeha %in% 'Not Available')
-            haname<-subset(stateset[,2],stateset$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack==as.character(min(as.numeric(outcomehaclean))))
+            outcomehaclean<-subset(outcomeha,!outcomeha %in% 'not available')
+            haname<-subset(stateset[,2],stateset$hospital.30.day.death..mortality..rates.from.heart.attack==as.character(min(as.numeric(outcomehaclean))))
             return(haname)
         }
         else if (outcome=='heart failure'){
             outcomehf<-stateset[,17]
-            outcomehfclean<-subset(outcomehf,!outcomehf %in% 'Not Available')
-            hfname<-subse(stateset[,2],stateset$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure==as.character(min(as.numeric(outcomehfclean))))
+            outcomehfclean<-subset(outcomehf,!outcomehf %in% 'not available')
+            hfname<-subset(stateset[,2],stateset$hospital.30.day.death..mortality..rates.from.heart.failure==as.character(min(as.numeric(outcomehfclean))))
             return(hfname)
         }
         else if (outcome=='pneumonia'){
             outcomep<-stateset[,23]
-            outcomepclean<-subset(outcomep,!outcomep %in% 'Not Available')
-            pname<-subset(stateset[,2],stateset$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia==as.character(min(as.numeric(outcomepclean))))
+            outcomepclean<-subset(outcomep,!outcomep %in% 'not available')
+            pname<-subset(stateset[,2],stateset$hospital.30.day.death..mortality..rates.from.pneumonia==as.character(min(as.numeric(outcomepclean))))
             return(pname)
         }
         else {
--- ProgAssignment3-data/rankhospital.R
+++ ProgAssignment3-data/rankhospital.R
@@ -12,31 +12,57 @@ rankhospital<-function(state,outcome,num='best'){
             outcomehaclean<-subset(outcomeha,!outcomeha %in% 'Not Available')
             statesetfull<-subset(stateset,!stateset[,11] %in% 'Not Available')
             if (num=='best'){
-            haname<-subset(stateset[,2],stateset$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack==as.character(min(as.numeric(outcomehaclean))))
-            return(haname)
+            haname<-subset(statesetfull[,2],as.numeric(statesetfull$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack)==min(as.numeric(outcomehaclean)))
+            return(sort(haname)[1])
             }
             else if (num=='worst'){
-            haname<-subset(stateset[,2],stateset$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack==as.character(max(as.numeric(outcomehaclean))))
-            return(haname)
+            haname<-subset(statesetfull[,2],as.numeric(statesetfull$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack)==max(as.numeric(outcomehaclean)))
+            return(sort(haname)[1])
             }
             else {
             set_rank<-cbind(statesetfull[,2],factor(statesetfull[,11]))
             colnames(set_rank)<-c('hospital','rank')
             haname<-set_rank[set_rank[,'rank']==num,1]
-            return(haname)
+            return(sort(haname)[1])
             }
         }
         else if (outcome=='heart failure'){
             outcomehf<-stateset[,17]
             outcomehfclean<-subset(outcomehf,!outcomehf %in% 'Not Available')
-            hfname<-subse(stateset[,2],stateset$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure==as.character(min(as.numeric(outcomehfclean))))
-            return(hfname)
+            statesetfullhf<-subset(stateset,!stateset[,17] %in% 'Not Available')
+            if (num=='best'){
+            hfname<-subset(statesetfullhf[,2],as.numeric(statesetfullhf$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure)==min(as.numeric(outcomehfclean)))
+            return(sort(hfname)[1])
+            }
+            else if (num=='worst'){
+            hfname<-subset(statesetfullhf[,2],as.numeric(statesetfullhf$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure)==max(as.numeric(outcomehfclean)))
+            return(sort(hfname)[1])
+            }
+            else {
+            set_rank_hf<-cbind(statesetfullhf[,2],factor(statesetfullhf[,17]))
+            colnames(set_rank_hf)<-c('hospital','rank')
+            hfname<-set_rank_hf[set_rank_hf[,'rank']==num,1]
+            return(sort(hfname)[1])
+            }
         }
         else if (outcome=='pneumonia'){
             outcomep<-stateset[,23]
             outcomepclean<-subset(outcomep,!outcomep %in% 'Not Available')
-            pname<-subset(stateset[,2],stateset$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia==as.character(min(as.numeric(outcomepclean))))
-            return(pname)
+            statesetfullp<-subset(stateset,!stateset[,23] %in% 'Not Available')
+            if (num=='best'){
+            pname<-subset(statesetfullp[,2],as.numeric(statesetfullp$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia)==min(as.numeric(outcomepclean)))
+            return(sort(pname)[1])
+            }
+            else if (num=='worst'){
+            pname<-subset(statesetfullp[,2],as.numeric(statesetfullp$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia)==max(as.numeric(outcomepclean)))
+            return(sort(pname)[1])
+        }
+            else {
+            set_rank_p<-cbind(statesetfullp[,2],factor(statesetfullp[,17]))
+            colnames(set_rank_p)<-c('hospital','rank')
+            pname<-set_rank_p[set_rank_p[,'rank']==num,1]
+            return(sort(pname)[1])
+            }
         }
         else {
        }
